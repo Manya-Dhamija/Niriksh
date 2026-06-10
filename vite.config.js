5b2034af457ea8a0d80ec3b04a1624df930805dc
Manya-Dhamija <manyadhamija5@gmail.com>
2026-06-10 18:29:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Change 'orbit-os' to your actual GitHub repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/orbit-os/',
+  base: '/Niriksh/',
 })
